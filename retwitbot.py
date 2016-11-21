3a03c16f87453e87276f3add8d2a45031bdf4ebc
Chris Hardee <shazzner@gmail.com>
2016-11-20 18:03:09 -0600

    Quick working twitterbot

--- retwitbot.py
+++ retwitbot.py
@@ -2,14 +2,11 @@
 # -*- coding: utf-8 -*-
  
 import tweepy, time, sys
+from apikeys import *
  
 argfile = str(sys.argv[1])
  
 #enter the corresponding information from your Twitter application:
-CONSUMER_KEY = '1234abcd...'#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = '1234abcd...'#keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = '1234abcd...'#keep the quotes, replace this with your access token
-ACCESS_SECRET = '1234abcd...'#keep the quotes, replace this with your access token secret
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
